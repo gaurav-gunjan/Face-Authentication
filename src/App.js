b70f2cc6cba0c4094341199d6258ae3d2331968e
gaurav-gunjan <ksbmksbm34@gmail.com>
2024-10-26 12:18:02 +0530

    Routing Added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
+import { Route, Routes } from "react-router-dom";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+
+// TODO : Components
 import FaceComponent from './components/FaceComponent';
+
+//! Lazy Load Pages
+const TermsOfService = lazy(() => import('./pages/terms-of-service'));
+const PrivacyPolicy = lazy(() => import('./pages/privacy-policy'));
 
 const App = () => {
   return (
     <>
-      <FaceComponent />
-
+      <Suspense fallback={<div className='flex justify-center items-center min-h-screen min-w-full text-lg'>Loading...</div>}>
+        <Routes>
+          <Route path='/' element={<FaceComponent />} />
+          <Route path='/terms-of-service' element={<TermsOfService />} />
+          <Route path='/privacy-policy' element={<PrivacyPolicy />} />
+        </Routes>
+      </Suspense>
       <ToastContainer />
     </>
   )
---
+++ src/pages/terms-of-service/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TermsOfService = () => {
+    return (
+        <>
+            <div className='flex justify-center items-center min-h-screen min-w-full text-lg'>Terms Of Services</div>
+        </>
+    )
+}
+
+export default TermsOfService;
